0c5bb332d561bffab5850b1729b4e1bab5bebfa1
Basheer Becerra <bbecerr@outlook.com>
2015-01-27 00:10:33 -0600

    First Commit!

---
+++ ClubGame/src/Main/TestFile.java
@@ -0,0 +1,11 @@
+package Main;
+
+public class TestFile {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		TestObject test = new TestObject();
+		test.printTest();
+	}
+
+}
---
+++ ClubGame/src/Main/TestObject.java
@@ -0,0 +1,12 @@
+package Main;
+
+public class TestObject {
+
+	public TestObject(){
+		
+	}
+	
+	public void printTest(){
+		System.out.println("It Works!");
+	}
+}
